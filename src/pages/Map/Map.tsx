857cce4c7173061ed168667798f7b0353b6c21ca
Saroj Thakur <sarojthakur247@gmail.com>
2021-11-12 22:26:05 +0530

    map integrated

--- src/pages/Map/Map.tsx
+++ src/pages/Map/Map.tsx
@@ -1,9 +1,26 @@
-const Map = () => {
-    return (
-        <div>
-            <h1>I'm Map</h1>
-        </div>
-    )
-}
+import GoogleMapReact from "google-map-react";
 
-export default Map
+const Map = () => {
+  const AnyReactComponent: React.FC<{
+    lat: number;
+    lng: number;
+    text: string;
+  }> = ({ lat, lng, text }) => <div>{text}</div>;
+
+  return (
+    <div style={{ height: "87vh", width: "100%" }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyCxB4NOyF5Du4Ih88qXosmZHq4HVCAG_yk" }}
+        defaultCenter={{
+          lat: 59.95,
+          lng: 30.33,
+        }}
+        defaultZoom={11}
+      >
+        <AnyReactComponent lat={59.955413} lng={30.337844} text="Saroj" />
+      </GoogleMapReact>
+    </div>
+  );
+};
+
+export default Map;
